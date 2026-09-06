dac830e2651b4ef9023a6b585889f1644acddeac
Zir.Abdirahman abdirashiid <abdirashidabdirahman439@gmail.com>
2026-09-06 09:39:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
-let menuIcon = document.querySelector('#menu-icon');
-        let navbar = document.querySelector('.navbar');
+// ================= MENU ICON =================
 
-        menuIcon.onclick = () => {
-            menuIcon.classList.toggle('bx-x');
-            navbar.classList.toggle('active');
-        };
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-        window.onscroll = () => {
-            menuIcon.classList.remove('bx-x');
-            navbar.classList.remove('active');
-        };
+
+// Open / Close Mobile Menu
+
+menuIcon.onclick = () => {
+
+    menuIcon.classList.toggle("bx-x");
+
+    navbar.classList.toggle("active");
+
+};
+
+
+// ================= SCROLL =================
+
+window.onscroll = () => {
+
+    menuIcon.classList.remove("bx-x");
+
+    navbar.classList.remove("active");
+
+};
+
+
+// ================= NAVBAR ACTIVE LINK =================
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+
+    let top = window.scrollY;
+
+    sections.forEach(section => {
+
+        let offset = section.offsetTop - 150;
+
+        let height = section.offsetHeight;
+
+        let id = section.getAttribute("id");
+
+        if (
+            top >= offset &&
+            top < offset + height
+        ) {
+
+            navLinks.forEach(link => {
+
+                link.classList.remove("active");
+
+                let activeLink =
+                    document.querySelector(
+                        '.navbar a[href*="' + id + '"]'
+                    );
+
+                if (activeLink) {
+                    activeLink.classList.add("active");
+                }
+
+            });
+
+        }
+
+    });
+
+});
